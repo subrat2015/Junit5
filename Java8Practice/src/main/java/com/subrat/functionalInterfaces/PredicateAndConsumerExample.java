fb881d793c3e25a3a643056155a983171a4d778c
Subrat Parida <sparida@marqeta.com>
2022-03-20 06:22:52 -0700

    few more fuctional interfaces.

---
+++ Java8Practice/src/main/java/com/subrat/functionalInterfaces/PredicateAndConsumerExample.java
@@ -0,0 +1,36 @@
+package com.subrat.functionalInterfaces;
+
+import com.subrat.data.Student;
+import com.subrat.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class PredicateAndConsumerExample {
+
+    static Predicate<Student> p1 = (s) -> s.getGradeLevel() >= 3;
+    static Predicate<Student> p2 = s1 -> s1.getGpa() >= 3.9;
+
+    BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name+" : "+activities);
+    Consumer<Student> studentConsumer = student -> {
+        if(p1.and(p1).test(student)){
+            studentBiConsumer.accept(student.getName(), student.getActivities());
+        }
+    };
+    Consumer<Student> studentConsumer1 = student -> {
+        if(p1.and(p1).test(student)){
+            studentBiConsumer.accept(student.getName(), student.getActivities());
+        }
+    };
+
+    public void printNameandActivities(List<Student> studentList){
+        studentList.forEach(studentConsumer);
+    }
+
+    public static void main(String[] args) {
+        List<Student> studentList = StudentDataBase.getAllStudents();
+        new PredicateAndConsumerExample().printNameandActivities(studentList);
+    }
+}
