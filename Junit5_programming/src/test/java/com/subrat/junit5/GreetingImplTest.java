2626bb14cd7f1c5dd5e34aedd4d28a00227f7862
Subrat Parida <sparida@marqeta.com>
2022-04-17 09:03:05 -0700

    junit5 test

---
+++ Junit5_programming/src/test/java/com/subrat/junit5/GreetingImplTest.java
@@ -0,0 +1,53 @@
+package com.subrat.junit5;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+public class GreetingImplTest {
+
+    private Greeting greeting;
+
+    @BeforeEach
+    public void setup() {
+        System.out.println("Setup");
+        greeting = new GreetingImpl();
+
+    }
+
+    @Test
+    public void greetShouldRetunAValidOutput() {
+        System.out.println("greetShouldRetunAValidOutput");
+        String result = greeting.greet("Junit");
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals("Hello Junit", result);
+    }
+
+    @Test
+    public void greetShouldThrowAnException_For_NameIsNull() {
+        System.out.println("greetShouldThrowAnException_For_NameIsNull");
+
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            greeting.greet(null);
+        });
+    }
+
+    @Test
+    public void greetShouldThrowAnException_For_NameIsBlank() {
+        System.out.println("greetShouldThrowAnException_For_NameIsBlank");
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            greeting.greet("");
+        });
+    }
+
+    @AfterEach
+    public void teardown() {
+        System.out.println("teardown");
+        greeting = null;
+    }
+
+}
